da836e606ef30efc67aef06406098db28a3c1bb7
Alexandre R. Saccol <arsaccol@gmail.com>
2019-06-04 22:33:03 -0300

    First commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float ViewDistance = 1f;
+    public float AlertTime = 10f;
+    public float FieldOfView = 5f;
+
+
+    private Animator sirenAnimator;
+
+    private void Start()
+    {
+        sirenAnimator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        //Debug.DrawRay(transform.position, transform.forward * ViewDistance, new Color(0xca / 255f, 0x2c / 255f, 0x92 / 255f));
+
+        //checkSpottedPlayerStraightForward();
+        checkSpottedPlayerWithAngle();
+    }
+
+    IEnumerator AlarmWait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        sirenAnimator.SetBool("IsSeeingPlayer", false);
+    }
+
+
+    private void checkSpottedPlayerWithAngle()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        //Debug.DrawRay(transform.position, player.transform.position - transform.position);
+        Debug.DrawRay(transform.position, player.transform.position);
+
+        //var player = GameObject.FindGameObjectWithTag("Player");
+
+        //var hereToPlayer = player.transform.position - transform.position;
+        //var hereToForward = transform.forward - transform.position;
+
+        //var playerViewAngle = Vector3.Angle(transform.forward, hereToPlayer - transform.position);
+
+
+        //Debug.Log(name + ": " + playerViewAngle.ToString());
+
+        //if(playerViewAngle <= FieldOfView && Vector3.Distance(player.transform.position, transform.position) <= ViewDistance)
+        //{
+        //    alarm();
+        //}
+
+        //Debug.DrawRay(transform.position, hereToPlayer, Color.red);
+        //Debug.DrawRay(transform.position, transform.forward * ViewDistance, Color.blue);
+    }
+
+
+    private void alarm()
+    {
+        if(sirenAnimator.GetBool("IsSeeingPlayer") == false)
+        {
+            sirenAnimator.SetBool("IsSeeingPlayer", true);
+            StartCoroutine(AlarmWait(AlertTime));
+        }
+    }
+
+
+    private void checkSpottedPlayerStraightForward()
+    {
+        RaycastHit hit;
+        Ray visionCenterRay = new Ray(transform.position, transform.forward);
+
+        if(Physics.Raycast(visionCenterRay, out hit, ViewDistance))
+        {
+            if(hit.collider.tag == "Player")
+            {
+                alarm();
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    private Transform m_cameraTransform;
+    public float m_CameraSensitity;
+
+    public float m_MotionSpeed = 10f;
+
+    private bool m_seen;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_cameraTransform = transform.GetComponentInChildren<Camera>().transform;
+        m_cameraTransform.GetComponent<Camera>().fieldOfView = 80f;
+        m_CameraSensitity = 200f;
+
+        m_seen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        toggle_seen();
+        wasd_motion(v, h);
+    }
+
+    private void LateUpdate()
+    {
+        float x = Input.GetAxis("Mouse X") * Time.deltaTime;
+        float y = -Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+        move_camera(x, y);
+    }
+
+    private void wasd_motion(float Vertical, float Horizontal)
+    {
+        var cameraPositionProjectedOntoFloor = Vector3.ProjectOnPlane(m_cameraTransform.position, Vector3.up);
+        var playerPositionProjectedOntoFloor = Vector3.ProjectOnPlane(transform.position, Vector3.up);
+
+        var differenceVector = (cameraPositionProjectedOntoFloor - playerPositionProjectedOntoFloor).normalized;
+
+        var forwardMotion = differenceVector * -Vertical * m_MotionSpeed * Time.deltaTime;
+        var sidewaysMotion = Vector3.Cross(differenceVector, Vector3.up) * Horizontal * m_MotionSpeed * Time.deltaTime; 
+        transform.position += forwardMotion + sidewaysMotion;
+    }
+
+    private void toggle_seen()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            m_seen = !m_seen;
+            foreach(var animatorObj in FindObjectsOfType<Animator>())
+            {
+                var animatorComponent = animatorObj.GetComponent<Animator>();
+
+                animatorComponent.SetBool("IsSeeingPlayer", m_seen);
+            }
+        }
+    }
+
+    private void move_camera(float X, float Y)
+    {
+        Vector3 cameraOffset = m_cameraTransform.position - transform.position;
+
+        Quaternion cameraXRotation = Quaternion.AngleAxis(X * m_CameraSensitity, transform.up);
+        Quaternion cameraYRotation = Quaternion.AngleAxis(Y * m_CameraSensitity, m_cameraTransform.right);
+        Vector3 newCameraPosition = cameraXRotation * cameraYRotation * cameraOffset;
+
+        m_cameraTransform.position = newCameraPosition;
+        newCameraPosition += transform.position;
+
+        m_cameraTransform.position = newCameraPosition;
+        m_cameraTransform.LookAt(transform.position);
+    }
+}
