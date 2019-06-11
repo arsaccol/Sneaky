1bfba83b154c3d5bb3ffa365c9dcc67e4e01bf4f
Alexandre R. Saccol <arsaccol@gmail.com>
2019-06-10 23:33:21 -0300

    Add waypoint class, waypoint gizmo icon and whatnot

---
+++ Assets/Scripts/Enemy/Waypoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public float WaitTime = 3f; // In seconds
+
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Gizmos.DrawSphere(transform.position, 0.1f);
+        Gizmos.DrawIcon(transform.position, "MapPinIcon", allowScaling: true);
+        
+    }
+}
