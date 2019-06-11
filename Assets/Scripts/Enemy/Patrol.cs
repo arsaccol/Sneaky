83503853bde2247b75adc2c17a6f3ee279a66749
Alexandre R. Saccol <arsaccol@gmail.com>
2019-06-11 00:10:20 -0300

    Add some rudimentary patrol

--- Assets/Scripts/Enemy/Patrol.cs
+++ Assets/Scripts/Enemy/Patrol.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class Patrol : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Waypoint[] Waypoints;
+    public bool Patrolling = true;
+    public int CurrentWaypointIndex = 0;
+
+
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+
     void Start()
     {
-        
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        //navMeshAgent.autoBraking = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.1f)
+        {
+            CurrentWaypointIndex = (CurrentWaypointIndex + 1) % Waypoints.Length;
+            navMeshAgent.SetDestination(Waypoints[CurrentWaypointIndex].transform.position);
+            StartCoroutine(WaitSeconds(Waypoints[CurrentWaypointIndex].WaitTime));
+        }
         
     }
+
+    IEnumerator WaitSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
 }
