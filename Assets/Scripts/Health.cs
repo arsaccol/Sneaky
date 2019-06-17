14595d61cba465d4c19dbddbf0a78f2381251675
Alexandre R. Saccol <arsaccol@gmail.com>
2019-06-17 08:47:05 -0300

    Current state of affairs, created weapon and health but incomplete

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float HitPoints = 100f;
+
+
+    public void InflictDamage(float howMuchDamage)
+    {
+        HitPoints -= howMuchDamage;
+        if(HitPoints <= 0f)
+            Die();
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int Ammunition;
+    public float RateOfFire;
+    public float Range = 50f;
+
+    public float WeaponDamage = 40f;
+
+    enum FiringType
+    {
+        BoltAction,
+        SemiAuto,
+        FullAuto,
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Fire1") > 0f)
+            Shoot();
+        
+    }
+
+    void Shoot()
+    {
+        Debug.DrawRay(transform.position, transform.forward * Range, Color.red);
+
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward * Range, out hit, Range))
+        {
+            Health health;
+            if(health = hit.transform.GetComponent<Health>())
+            {
+                health.InflictDamage(WeaponDamage);
+            }
+
+        }
+    }
+}
