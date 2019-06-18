347dd43a607236a2d9d76b76e71fc97f5a86833d
Alexandre R. Saccol <arsaccol@gmail.com>
2019-06-17 22:33:18 -0300

    Add some gibs and stuff

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -16,6 +16,25 @@ public class Health : MonoBehaviour
 
     public void Die()
     {
+        gib(100);
         gameObject.SetActive(false);
+    }
+
+    private void gib(int howManyGibs)
+    {
+        for(int i = 0; i < howManyGibs; ++i)
+        {
+            var newGib = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            newGib.transform.position = transform.position;
+            newGib.transform.localScale = new Vector3(.2f, .2f, .2f);
+
+            newGib.GetComponent<Renderer>().material.color = Color.red;
+            var gibPhysics = newGib.AddComponent<Rigidbody>();
+
+            newGib.AddComponent<Health>();
+            gibPhysics.AddForce(new Vector3(Random.Range(-100f, 100f), Random.Range(0f, 1f), Random.Range(-100f, 100f)));
+        }
+
+
     }
 }
